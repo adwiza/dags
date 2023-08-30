d15b7e9299759dcced4a77fb10635527a5345e56
Aleksei Elokhin <aleksei.e@clips4sale.com>
2023-08-30 07:35:39 +0300

    Add multiprocessing es_kafka

--- es_kafka.py
+++ es_kafka.py
@@ -1,52 +1,71 @@
 import json
-import time
+from multiprocessing import Process, cpu_count, Queue
 
-from elasticsearch import Elasticsearch
 from confluent_kafka import Producer
-import socket
-from config import ES_URL
-
+from elasticsearch import Elasticsearch
 from loguru import logger
 
-# Initialize Elasticsearch and Kafka connections
-es = Elasticsearch([ES_URL])
+from config import ES_URL
 
 
-resp = es.search(index="purchases-000001", size=1000, scroll='10m')
+def produce_messages(queue, kafka_config, topic):
+    producer = Producer(kafka_config)
 
-kafka_config = {
-    "bootstrap.servers": "10.31.68.81:9092",
-    "queue.buffering.max.messages": 100000,  # Adjust as needed
-    "queue.buffering.max.ms": 100,  # Adjust as needed
-    # Other Kafka configuration options
-}
+    while True:
+        message = queue.get()
+        if message is None:
+            break
 
-producer = Producer(kafka_config)
-
-old_scroll_id = resp['_scroll_id']
-
-while len(resp['hits']['hits']):
-
-    # # check if there's a new scroll ID
-    # if old_scroll_id != resp['_scroll_id']:
-    #     logger.info(f"NEW SCROLL ID: {resp['_scroll_id']}")
-    #     # pass
-
-    # keep track of pass scroll _id
-    old_scroll_id = resp['_scroll_id']
-    count = 0
-    for hit in resp["hits"]["hits"]:
-        kafka_message = hit["_source"]
-        # Convert dictionary data to JSON and then to bytes
-        message_value = json.dumps(kafka_message) #.encode("utf-8")
-        # logger.debug(message_value)
-        producer.produce("purchases-000001", value=message_value)
+        kafka_message = message["_source"]
+        message_value = json.dumps(kafka_message)
+        producer.produce(topic, value=message_value)
         producer.poll(0)  # Trigger delivery reports
 
-        count += 1
-        # Check delivery status and implement backpressure if needed
-        while producer.flush(1) > 0:  # Wait until all messages are delivered
-            time.sleep(1)  # Sleep for a while before checking again
-    if count == 1000:
-        logger.info(f"{count} records successfully processed")
-producer.flush()
+    producer.flush()
+
+
+def main():
+    es = Elasticsearch([ES_URL])
+
+    resp = es.search(index="purchases-000001", size=10000, scroll='10m')
+
+    kafka_config = {
+        "bootstrap.servers": "10.31.68.81:9092",
+        "queue.buffering.max.messages": 100000,  # Adjust as needed
+        "queue.buffering.max.ms": 100,  # Adjust as needed
+        # Other Kafka configuration options
+    }
+    kafka_topic = "purchases-000001"
+
+    message_queue = Queue()
+    processes = []
+
+    num_processes = cpu_count()  # Number of CPU cores available
+    for _ in range(num_processes):
+        process = Process(target=produce_messages, args=(message_queue, kafka_config, kafka_topic))
+        processes.append(process)
+        process.start()
+
+    while len(resp['hits']['hits']):
+        old_scroll_id = resp['_scroll_id']
+        count = 0
+
+        for hit in resp["hits"]["hits"]:
+            message_queue.put(hit)
+
+            count += 1
+
+        if count == 10000:
+            logger.info(f"{count} records successfully processed")
+
+        resp = es.scroll(scroll_id=old_scroll_id, scroll='10m')
+
+    # Signal processes to exit
+    for _ in range(num_processes):
+        message_queue.put(None)
+    for process in processes:
+        process.join()
+
+
+if __name__ == "__main__":
+    main()
